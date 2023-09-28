15ec3921187988b289b76fbfa5adc5d7aeba6247
SerlorsX_ <93506040+SerlorsX2@users.noreply.github.com>
2023-09-29 00:05:06 +0700

    Update client_uuid.py

--- client_uuid.py
+++ client_uuid.py
@@ -1,26 +1,31 @@
-from random import random
 from datetime import datetime
 from struct import pack
 from base64 import b64encode
-from time import mktime
+from random import randint
 
-user_id = int("your discord id")
-number = int(4294967296 * random())
-creation_time = int(mktime(datetime.now().timetuple()) * 1000)
 
-bytes1 = bytes(pack("<i", user_id % 4294967296 if user_id % 4294967296 <= 2147483647 else user_id % 4294967296 - 2147483647))
-bytes2 = bytes(pack("<i", user_id >> 32))
-bytes3 = bytes(pack("<i", number if number <= 2147483647 else number - 4294967296))
-bytes4 = bytes(pack("<i", creation_time % 4294967296 if creation_time % 4294967296 <= 2147483647 else creation_time % 4294967296 - 2147483647))
-bytes5 = bytes(pack("<i", creation_time >> 32))
-bytes6 = bytes(pack("<i", 0))
+def _get_creation_time():
+    return int(datetime.now().timestamp() * 1000)
 
-bytearray = bytearray(pack('24x'))
-bytearray[0: len(bytes1)] = bytes1
-bytearray[4:4 + len(bytes2)] = bytes2
-bytearray[8:8 + len(bytes3)] = bytes3
-bytearray[12:12 + len(bytes4)] = bytes4
-bytearray[16:16 + len(bytes5)] = bytes5
-bytearray[20:20 + len(bytes6)] = bytes6
+def _find_client_uuid(user_id: int):
+    global bytearray
+    
+    creation_time = _get_creation_time()
+    bytearray = bytearray(pack("<6i", *[
+        user_id % 2**31, 
+        user_id >> 32, 
+        randint(0, 4294967295) % 2**31, 
+        creation_time % 2**31, 
+        creation_time >> 32, 0
+    ]))
 
-print(b64encode(bytearray).decode('ascii'))
+    return b64encode(bytearray).decode("ascii")
+
+
+if (__name__ == '__main__'):
+    user_id = 803241947954151459
+    client_uuid = _find_client_uuid(
+        user_id=user_id
+    )
+    
+    print(client_uuid)
